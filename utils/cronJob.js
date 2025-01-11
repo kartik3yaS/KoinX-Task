8efc1cfd5fada0cde498286ed7ff0ed7456ebfad
kartik3yaS <kartikeyashukla009@gmail.com>
2025-01-11 16:17:32 +0530

    Add cron job using node-cron to schedule periodic tasks

--- utils/cronJob.js
+++ utils/cronJob.js
@@ -1,14 +1,15 @@
+const cron = require("node-cron");
 const { fetchCryptoData } = require("../services/cryptoService");
 
 const startCronJob = () => {
-  setInterval(async () => {
+  cron.schedule("0 */2 * * *", async () => {
     console.log("Cron job is running...");
     try {
       await fetchCryptoData();
     } catch (error) {
       console.error("Error in cron job execution:", error);
     }
-  }, 7200000);
+  });
 
   console.log("Cron job scheduled to run every 2 hours.");
 };
